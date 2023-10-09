2f54f2905de96378f5fa0975dd5383bcf347294c
DianaShylova <133662276+DianaShylova@users.noreply.github.com>
2023-10-09 15:18:07 +0000

    Deploying to gh-pages from @ DianaShylova/waw-car-rentals@b154778b35eba7f7e9dbfc0f0e02ad2e862290b0 🚀

---
+++ static/js/266.9cfff59f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,o){o.r(t),o.d(t,{default:function(){return n}});var r=o(3329);function n(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{style:{textAlign:"center",fontSize:"36px",fontWeight:"bolder",marginTop:"30px",width:"350px",marginLeft:"auto",marginRight:"auto",color:"rgb(102, 51, 0)"},children:"Welcome to your phonebook!"})})}}}]);
+//# sourceMappingURL=266.9cfff59f.chunk.js.map
